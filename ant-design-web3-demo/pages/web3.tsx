29c417c212dd93875ac27adc7a50996c05571057
Terry <ytdxxt10@163.com>
2025-07-11 16:50:20 +0800

    fix:部署智能合约到测试链并调用合约

--- ant-design-web3-demo/pages/web3.tsx
+++ ant-design-web3-demo/pages/web3.tsx
@@ -1,15 +1,31 @@
 
-import { Mainnet, WagmiWeb3ConfigProvider,MetaMask } from '@ant-design/web3-wagmi';
-import { http,useReadContract,useWriteContract } from "wagmi";
+import { Mainnet, WagmiWeb3ConfigProvider,MetaMask,Sepolia } from '@ant-design/web3-wagmi';
+import { http,useReadContract,useWriteContract,createConfig } from "wagmi";
 import { Address,NFTCard,Connector,ConnectButton,useAccount } from '@ant-design/web3';
 import {parseEther} from 'viem';
-import { Button } from 'antd';
+import { mainnet,sepolia } from 'viem/chains';
+import { injected } from "wagmi/connectors";
+import { Button, message } from "antd";
+
+
+const config = createConfig({
+  chains: [mainnet, sepolia],
+  transports: {
+     [mainnet.id]: http(),
+    [sepolia.id]: http(),
+  },
+  connectors: [
+    injected({
+      target: "metaMask",
+    }),
+  ],
+});
 
 const CallTest = () => {
     const {writeContract} = useWriteContract();
     const { account } = useAccount();
     const { data, isLoading } = useReadContract({
-        address: '0xEcd0D12E21805803f70de03B72B1C162dB0898d9',
+        address: '0x83b50abf491b150bc91b4a4436a9af0c1c8c24a5',
         abi: [
             {
                 "inputs": [{name:'account', type: 'address'}],
@@ -30,10 +46,10 @@ const CallTest = () => {
     return (
         <div>
             <p>Data: {isLoading ? 'Loading...' : data?.toString()}</p>
-            <button
+            <Button
                 onClick={() => {
                     writeContract({
-                        address: '0xEcd0D12E21805803f70de03B72B1C162dB0898d9',
+                        address: '0x83b50abf491b150bc91b4a4436a9af0c1c8c24a5',
                         abi: [
                             {
                                 "inputs": [{name:'quantity', type: 'uint256',internalType: 'uint256'}],
@@ -58,7 +74,7 @@ const CallTest = () => {
                 }}
                 >
                     mint
-                </button>
+                </Button>
 
         </div>
     );
@@ -66,13 +82,14 @@ const CallTest = () => {
 export default function Web3() {
     return(
         <WagmiWeb3ConfigProvider
-            chains={[Mainnet]} 
+            config={config}
+            chains={[Sepolia]} 
             transports={{[Mainnet.id]: http('https://api.zan.top/node/v1/eth/sepolia/fe682edfa4e44534b33b93a0483ecd53')}}
             wallets={[MetaMask()]}
             >
-            <Address format address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9" />
+            <Address format address="0x83b50abf491b150bc91b4a4436a9af0c1c8c24a5" />
             <NFTCard
-                address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9"
+                address="0x83b50abf491b150bc91b4a4436a9af0c1c8c24a5"
                 tokenId={641}
                 />
                 <Connector>
