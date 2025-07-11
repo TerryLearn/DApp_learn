514be4abdef9cc6149d7e2ca0112e6df87f1d60f
Terry <ytdxxt10@163.com>
2025-07-11 10:25:25 +0800

    feat:链接小狐狸钱包

--- ant-design-web3-demo/pages/web3.tsx
+++ ant-design-web3-demo/pages/web3.tsx
@@ -1,19 +1,23 @@
 
-import { Mainnet, WagmiWeb3ConfigProvider } from '@ant-design/web3-wagmi';
+import { Mainnet, WagmiWeb3ConfigProvider,MetaMask } from '@ant-design/web3-wagmi';
 import { http } from "wagmi";
-import { Address,NFTCard } from '@ant-design/web3';
+import { Address,NFTCard,Connector,ConnectButton } from '@ant-design/web3';
 
 export default function Web3() {
     return(
         <WagmiWeb3ConfigProvider
             chains={[Mainnet]} 
             transports={{[Mainnet.id]: http()}}
+            wallets={[MetaMask()]}
             >
             <Address format address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9" />
             <NFTCard
                 address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9"
                 tokenId={641}
                 />
+                <Connector>
+                    <ConnectButton />
+                </Connector>
             </WagmiWeb3ConfigProvider>  
 
     )
