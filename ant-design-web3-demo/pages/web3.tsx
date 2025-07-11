12a56a2132d8252160dbf72c3b3eabe5bc70721c
Terry <ytdxxt10@163.com>
2025-07-11 11:04:53 +0800

    feat:调取智能合约 并mint

--- ant-design-web3-demo/pages/web3.tsx
+++ ant-design-web3-demo/pages/web3.tsx
@@ -1,8 +1,68 @@
 
 import { Mainnet, WagmiWeb3ConfigProvider,MetaMask } from '@ant-design/web3-wagmi';
-import { http } from "wagmi";
-import { Address,NFTCard,Connector,ConnectButton } from '@ant-design/web3';
+import { http,useReadContract,useWriteContract } from "wagmi";
+import { Address,NFTCard,Connector,ConnectButton,useAccount } from '@ant-design/web3';
+import {parseEther} from 'viem';
+import { Button } from 'antd';
 
+const CallTest = () => {
+    const {writeContract} = useWriteContract();
+    const { account } = useAccount();
+    const { data, isLoading } = useReadContract({
+        address: '0xEcd0D12E21805803f70de03B72B1C162dB0898d9',
+        abi: [
+            {
+                "inputs": [{name:'account', type: 'address'}],
+                "name": "balanceOf",
+                "outputs": [
+                    {
+                        "type": "uint256"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            }
+        ],
+        functionName: 'balanceOf',
+        args: [account?.address as `0x${string}`],
+    });
+
+    return (
+        <div>
+            <p>Data: {isLoading ? 'Loading...' : data?.toString()}</p>
+            <button
+                onClick={() => {
+                    writeContract({
+                        address: '0xEcd0D12E21805803f70de03B72B1C162dB0898d9',
+                        abi: [
+                            {
+                                "inputs": [{name:'quantity', type: 'uint256',internalType: 'uint256'}],
+                                "name": "mint",
+                                "outputs": [],
+                                "stateMutability": "payable",
+                                "type": "function"
+                            }
+                        ],
+                        functionName: 'mint',
+                        args: [BigInt(1)],
+                        value: parseEther('0.01'),
+                    },
+                    {
+                        onSuccess: (data) => {
+                            console.log('Mint successful:', data);
+                        },
+                        onError: (error) => {
+                            console.error('mint failed:', error);
+                    }
+                });
+                }}
+                >
+                    mint
+                </button>
+
+        </div>
+    );
+}
 export default function Web3() {
     return(
         <WagmiWeb3ConfigProvider
@@ -18,6 +78,7 @@ export default function Web3() {
                 <Connector>
                     <ConnectButton />
                 </Connector>
+                <CallTest />
             </WagmiWeb3ConfigProvider>  
 
     )
