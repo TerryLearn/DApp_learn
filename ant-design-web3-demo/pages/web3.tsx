1e2f697565fc3d6329703b988e0d68e815c6e044
Terry <ytdxxt10@163.com>
2025-07-11 18:14:35 +0800

    fix:多条链切换

--- ant-design-web3-demo/pages/web3.tsx
+++ ant-design-web3-demo/pages/web3.tsx
@@ -1,18 +1,19 @@
 
-import { Mainnet, WagmiWeb3ConfigProvider,MetaMask,Sepolia,WalletConnect } from '@ant-design/web3-wagmi';
+import { Mainnet, WagmiWeb3ConfigProvider,MetaMask,Sepolia,WalletConnect,Polygon } from '@ant-design/web3-wagmi';
 import { http,useReadContract,useWriteContract,createConfig } from "wagmi";
-import { Address,NFTCard,Connector,ConnectButton,useAccount } from '@ant-design/web3';
+import { Address,NFTCard,Connector,ConnectButton,useAccount,useProvider } from '@ant-design/web3';
 import {parseEther} from 'viem';
-import { mainnet,sepolia } from 'viem/chains';
+import { mainnet,sepolia,polygon} from 'viem/chains';
 import { injected,walletConnect } from "wagmi/connectors";
 import { Button, message } from "antd";
 
 
 const config = createConfig({
-  chains: [mainnet, sepolia],
+  chains: [mainnet, sepolia,polygon],
   transports: {
      [mainnet.id]: http(),
     [sepolia.id]: http(),
+    [polygon.id]: http()
   },
   connectors: [
     injected({
@@ -25,11 +26,28 @@ const config = createConfig({
   ],
 });
 
+ const contractInfo = [
+  {
+    id:1,
+    name: "Ethereum",
+    contractAddress: "0xEcd0D12E21805803f70de03B72B1C162dB0898d9"
+  }, {
+    id:5,
+    name: "Sepolia",
+    contractAddress: "0x83b50abf491b150bc91b4a4436a9af0c1c8c24a5"
+  }, {
+    id:137,
+    name: "Polygon",
+    contractAddress: "0x83b50abf491b150bc91b4a4436a9af0c1c8c24a5"
+  }
+ ]
+
 const CallTest = () => {
     const {writeContract} = useWriteContract();
     const { account } = useAccount();
+    const { chain} = useProvider();
     const { data, isLoading } = useReadContract({
-        address: '0x83b50abf491b150bc91b4a4436a9af0c1c8c24a5',
+    address: contractInfo.find((item) => item.id === chain?.id)?.contractAddress as `0x${string}`,
         abi: [
             {
                 "inputs": [{name:'account', type: 'address'}],
@@ -53,7 +71,7 @@ const CallTest = () => {
             <Button
                 onClick={() => {
                     writeContract({
-                        address: '0x83b50abf491b150bc91b4a4436a9af0c1c8c24a5',
+                       address: contractInfo.find((item) => item.id === chain?.id)?.contractAddress as `0x${string}`,
                         abi: [
                             {
                                 "inputs": [{name:'quantity', type: 'uint256',internalType: 'uint256'}],
@@ -87,7 +105,7 @@ export default function Web3() {
     return(
         <WagmiWeb3ConfigProvider
             config={config}
-            chains={[Sepolia]} 
+            chains={[Sepolia, Polygon]} 
             transports={{[Mainnet.id]: http('https://api.zan.top/node/v1/eth/sepolia/fe682edfa4e44534b33b93a0483ecd53')}}
             wallets={[MetaMask(),WalletConnect()]}
             eip6963={{
